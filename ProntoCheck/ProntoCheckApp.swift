a546ea64b2e53e6f5f6b200bfeee03a83190add0
vicente <isc.luisangel@gmail.com>
2026-06-24 09:27:51 -0500

    Initial Commit

---
+++ ProntoCheck/ProntoCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProntoCheckApp.swift
+//  ProntoCheck
+//
+//  Created by usuario on 24/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ProntoCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
